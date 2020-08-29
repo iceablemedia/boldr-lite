0ff9d5468c58515f42abb66af18b5a39a2e18948
Mathieu Sarrasin <mathieu@iceable.com>
2020-08-29 14:31:52 +0200

    Fixes archive title escaping for WP > 5.5

--- index.php
+++ index.php
@@ -30,7 +30,7 @@ get_header();
 	/* ARCHIVE CONDITIONAL TITLE */
 	if ( is_archive() ) :
 		?>
-		<h1 class="page-title"><?php echo esc_html( get_the_archive_title() ); ?></h1>
+		<h1 class="page-title"><?php echo wp_kses( get_the_archive_title(), 'post' ); ?></h1>
 		<?php
 	endif;
 
